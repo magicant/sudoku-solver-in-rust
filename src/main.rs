385cfbad3ad26723b7ccf9c17c191ab750ff45f4
WATANABE Yuki <magicant@wonderwand.net>
2020-11-28 19:23:33 +0900

    Reject malformed problem

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,25 @@ mod solver;
 
 use board::*;
 use solver::for_each_solution;
+use std::io::stdin;
 use std::io::BufRead;
 use std::io::Error;
 use std::io::ErrorKind;
 use std::io::Result;
 
+fn eof() -> Error {
+    Error::new(ErrorKind::UnexpectedEof, "malformed problem")
+}
+
 fn read_problem() -> Result<Board<Option<usize>>> {
     let mut board = Board([[None; N]; N]);
-    for (i, line) in std::io::stdin().lock().lines().take(N).enumerate() {
-        let line = line?;
-        for (j, n) in line
-            .chars()
-            .filter_map(|c| c.to_digit(10))
-            .take(N)
-            .enumerate()
-        {
+    let input = stdin();
+    let mut lines = input.lock().lines();
+    for i in 0..N {
+        let line = lines.next().unwrap_or_else(|| Err(eof()))?;
+        let mut line = line.chars().filter_map(|c| c.to_digit(10));
+        for j in 0..N {
+            let n = line.next().ok_or_else(eof)?;
             board.0[i][j] = if n == 0 { None } else { Some((n - 1) as usize) }
         }
     }
